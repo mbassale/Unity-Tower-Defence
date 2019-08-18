f3c6938b09abbba91489bddf8b39028bbea30edf
Marco Bassaletti <bassaletti@gmail.com>
2019-08-18 10:29:30 -0400

    Initial Commit: Tower Defence game

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [Range(1, 20)] [SerializeField] int gridSize = 10;
+    [Range(1, 20)] [SerializeField] int cellSize = 10;
+    TextMesh textMesh;
+    
+    void Update()
+    {
+        Vector3 snapPosition;
+        snapPosition.y = 0;
+        var worldSize = gridSize * cellSize;
+        snapPosition.x = Mathf.RoundToInt(Mathf.Clamp(transform.position.x, -worldSize, worldSize) / cellSize) * cellSize;
+        snapPosition.z = Mathf.RoundToInt(Mathf.Clamp(transform.position.z, -worldSize, worldSize) / cellSize) * cellSize;
+        transform.position = snapPosition;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = (snapPosition.x / cellSize).ToString() + "," + (snapPosition.z / cellSize).ToString();
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+}
