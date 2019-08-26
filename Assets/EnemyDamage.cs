ff24974a45b90a25d070738a928c4b1bde865887
Marco Bassaletti <bassaletti@gmail.com>
2019-08-25 20:49:37 -0400

    Created towers, support for towers to shoot enemies. Enemy hitpoints, when hitpoints reach zero, destroy the enemy.

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print("I'm hit");
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        hitPoints = hitPoints - 1;
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
