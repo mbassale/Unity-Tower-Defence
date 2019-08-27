8914f724a100d5bfbd0f5381ef28a7d33b0b98ae
Marco Bassaletti <bassaletti@gmail.com>
2019-08-27 01:12:01 -0400

    Play hit particle when enemy reaches the base.

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -6,18 +6,13 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float movementPeriod = 1f;
+    [SerializeField] ParticleSystem goalParticle;
 
     // Start is called before the first frame update
     void Start()
     {
         var pathfinder = FindObjectOfType<Pathfinder>();
         StartCoroutine(PrintAllWaypoints(pathfinder.Path));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-     
     }
 
     private IEnumerator PrintAllWaypoints(List<Waypoint> path)
@@ -27,5 +22,18 @@ public class EnemyMovement : MonoBehaviour
             transform.position = waypoint.transform.position;
             yield return new WaitForSeconds(movementPeriod);
         }
+        SelfDestruct();
+    }
+
+    void SelfDestruct()
+    {
+        if (goalParticle)
+        {
+            var vfx = Instantiate(goalParticle, transform.position, Quaternion.identity);
+            vfx.Play();
+            float destroyDelay = vfx.main.duration;
+            Destroy(vfx.gameObject, destroyDelay);
+        }
+        Destroy(gameObject);
     }
 }
